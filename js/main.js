077630b601e8a7bb7c2109bdef33a2bbf6627439
onezeun <onezeun@gmail.com>
2022-12-13 17:55:33 +0900

    feat: 메인페이지 시너지 슬라이드 구현

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 $(document).ready(function () {
-  $('.gnb_ckbox').on('click', function(){
-    if ( $(this).prop('checked') ) {
-      $('.main_menu').animate({ right : '0'})
+  // 햄버거메뉴
+  $(".gnb_ckbox").on("click", function () {
+    if ($(this).prop("checked")) {
+      $(".main_menu").animate({ right: "0" });
     } else {
-      $('.main_menu').animate({ right : '-300px'})
+      $(".main_menu").animate({ right: "-300px" });
     }
   });
+  // 시너지 슬라이더
+  $(".syn_slide").not('.slick-initialized').slick({
+    infinite: true,
+    prevArrow: $('.syn_prev'),
+    nextArrow: $('.syn_next'),
+  });
+
+  $('.syn_symbol').hide();
+  $('.slick-active').find('.syn_symbol').fadeIn(200);
+
+  $(".syn_slide").on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    $('.slick-active').find('.syn_symbol').fadeOut(1000);
+  });
+
+  $(".syn_slide").on('afterChange', function (event, slick, currentSlide) {
+    $('.slick-active').find('.syn_symbol').fadeIn(200);
+  });
+
 });
