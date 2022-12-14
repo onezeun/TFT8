5bad790c8bbae0de43e95471012d686bc7a98c49
onezeun <onezeun@gmail.com>
2022-12-14 15:44:21 +0900

    feat: 영웅증강 슬라이드 구현

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
   // 시너지 슬라이더
   $(".syn_slide").not('.slick-initialized').slick({
     infinite: true,
-    prevArrow: $('.syn_prev'),
-    nextArrow: $('.syn_next'),
+    prevArrow: $('#syn_prev'),
+    nextArrow: $('#syn_next'),
   });
 
   $('.syn_symbol').hide();
@@ -23,6 +23,12 @@ $(document).ready(function () {
 
   $(".syn_slide").on('afterChange', function (event, slick, currentSlide) {
     $('.slick-active').find('.syn_symbol').fadeIn(200);
+  });
+
+  $(".agm_slide").not('.slick-initialized').slick({
+    infinite: true,
+    prevArrow: $('#agm_prev'),
+    nextArrow: $('#agm_next'),
   });
 
 });
