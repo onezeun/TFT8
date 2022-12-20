047d062529fa8fb975367b7a471d1a130d7db266
onezeun <onezeun@gmail.com>
2022-12-20 16:30:33 +0900

    feat: profile UI

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   // 차트
-  var chartArea = document.querySelector('canvas').getContext('2d');
+  var chartArea = document.querySelector("canvas").getContext("2d");
 
   // 차트를 생성한다.
   var myChart = new Chart(chartArea, {
@@ -9,18 +9,27 @@ $(document).ready(function () {
     // ②차트의 데이터(Object)
     data: {
       // ③x축에 들어갈 이름들(Array)
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: ["#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8"],
       // ④실제 차트에 표시할 데이터들(Array), dataset객체들을 담고 있다.
       datasets: [
         {
           // ⑤dataset의 이름(String)
-          label: "# of Votes",
+          label: "",
           // ⑥dataset값(Array)
-          data: [12, 19, 3, 5, 2, 3],
+          data: [3, 3, 1, 5, 2, 3, 0, 3],
           // ⑦dataset의 배경색(rgba값을 String으로 표현)
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          backgroundColor: [
+            "#1ca983",
+            "#3786c7",
+            "#3786c7",
+            "#3786c7",
+            "#dadada",
+            "#dadada",
+            "#dadada",
+            "#dadada",
+          ],
           // ⑧dataset의 선 색(rgba값을 String으로 표현)
-          borderColor: "rgba(255, 99, 132, 1)",
+          borderColor: "rgba(255,255,255,0)",
           // ⑨dataset의 선 두께(Number)
           borderWidth: 1,
         },
@@ -28,12 +37,16 @@ $(document).ready(function () {
     },
     // ⑩차트의 설정(Object)
     options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
       // ⑪축에 관한 설정(Object)
       scales: {
         // ⑫y축에 대한 설정(Object)
         y: {
-          // ⑬시작을 0부터 하게끔 설정(최소값이 0보다 크더라도)(boolean)
-          beginAtZero: true,
+          ticks: { beginAtZero: true, stepSize: 1 },
         },
       },
     },
