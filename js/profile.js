62867dfe327ffb0d2bac4a0e45073e34408f2015
onezeun <onezeun@gmail.com>
2022-12-22 18:45:17 +0900

    feat: 게임타입, 플레이시간 연동

--- js/profile.js
+++ js/profile.js
@@ -9,28 +9,64 @@ $(document).ready(function () {
   var participants;
   var history_rank = [];
   var rank_list = [];
+  var game_type;
+  var game_time;
+  var game_day;
+
+  //시간 구하기
+  const time = (seconds) => {
+    var min =
+      parseInt((seconds % 3600) / 60) < 10
+        ? "0" + parseInt((seconds % 3600) / 60)
+        : parseInt((seconds % 3600) / 60);
+    var sec =
+      parseInt(seconds % 60) < 10
+        ? "0" + parseInt(seconds % 60)
+        : parseInt(seconds % 60);
+    game_time = min + ":" + sec;
+  };
+
+  //날짜 구하기
+  function day(value) {
+    const today = new Date();
+    const timeValue = new Date(value);
+
+    const betweenTime = Math.floor(
+      (today.getTime() - timeValue.getTime()) / 1000 / 60
+    );
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+
+    if (betweenTime < 1) {
+      game_time = "방금전";
+    } else if (betweenTime > 1 && betweenTime < 60) {
+      game_time = `${betweenTime}분전`;
+    } else if (betweenTime > 60 && betweenTime < 1440) {
+      game_time = `${betweenTimeHour}시간전`;
+    } else if (betweenTime > 1440) {
+      game_time = `${betweenTimeDay}일전`;
+    }
+  }
 
   $.ajax({
-    url:"https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-name/" + search_name + "?api_key=" + api_key,
+    url:"https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-name/" +search_name +"?api_key=" +api_key,
     type: "GET",
     async: false,
     dataType: "json",
-    success : function() {
-      $('.content').show()
-      $('.err_box').hide()
-
+    success: function () {
+      $(".content").show();
+      $(".err_box").hide();
     },
-    error: function() {
-      console.log('에러')
-      $('#srch_txt').text(search_name)
-      $('.content').hide()
-      $('.err_box').show()
-    }
+    error: function () {
+      console.log("에러");
+      $("#srch_txt").text(search_name);
+      $(".content").hide();
+      $(".err_box").show();
+    },
   })
     .done(function (data) {
-
       $(".user_name").text(data.name);
-      $("#u_img").attr('src', 'http://ddragon.leagueoflegends.com/cdn/12.23.1/img/profileicon/'+data.profileIconId+'.png')
+      $("#u_img").attr("src","http://ddragon.leagueoflegends.com/cdn/12.23.1/img/profileicon/" +data.profileIconId +".png");
       user_id = data.id;
       puu_id = data.puuid;
     })
@@ -103,7 +139,7 @@ $(document).ready(function () {
         })
         .then(() => {
           $.ajax({
-            url:"https://asia.api.riotgames.com/tft/match/v1/matches/by-puuid/"+puu_id+"/ids?start=0&count=20&api_key="+api_key,
+            url:"https://asia.api.riotgames.com/tft/match/v1/matches/by-puuid/" +puu_id +"/ids?start=0&count=20&api_key=" +api_key,
             type: "GET",
             async: false,
             dataType: "json",
@@ -112,15 +148,29 @@ $(document).ready(function () {
             match_id = data;
           });
           match_id.map((game, i) => {
-            $.ajax({
-              url:"https://asia.api.riotgames.com/tft/match/v1/matches/" +game +"?api_key="+api_key,
-              type: "GET",
+            console.log(game)
+            $.ajax({url:"https://asia.api.riotgames.com/tft/match/v1/matches/" +game +"?api_key=" +api_key,type: "GET",
               async: false,
               dataType: "json",
             }).done(function (data) {
-              participants = data.info.participants;
-
-              participants.map((player, i) => {
+              switch (data.info.queue_id) {
+                case 1090:
+                  game_type = "일반";
+                  break;
+                case 1100:
+                  game_type = "랭크";
+                  break;
+                case 1130:
+                  game_type = "초고속모드";
+                  break;
+                case 1160:
+                  game_type = "더블업";
+                  break;
+              }
+              time(data.info.game_length);
+              // day(data.info.game_datetime);
+              // console.log(game_time);
+              participants.map(participants, (player, i) => {
                 if (player.puuid == puu_id) {
                   // 게임 참가자 중 검색한 유저의 정보만 배열에 넣기
                   // history_user.push(player)
@@ -143,6 +193,7 @@ $(document).ready(function () {
                       background = 'style="background:#ccc"';
                       break;
                   }
+
                   $(".chart_val").append(
                     '<div class="one_box"' +
                       background +
@@ -155,43 +206,49 @@ $(document).ready(function () {
                       background +
                       '>#<span class="hst_rank">' +
                       player.placement +
-                      '</span></div><div class="history_cont"><div class="summary"><span class="p_time">플레이타임</span><span class="p_day">플레이날짜</span></div><div class="history_flex_box"><div class="traits">특성</div><div class="augments">증강</div></div><div class="units">유닛</div></div></div>'
+                      '</span></div><div class="history_cont"><div class="summary"><span class="p_type">' +
+                      game_type +
+                      '</span> <span class="p_time">' +
+                      game_time +
+                      '</span> <span class="p_day">' +
+                      game_day +
+                      '</span></div><div class="history_flex_box"><div class="traits">특성</div><div class="augments">증강</div></div><div class="units">유닛</div></div></div>'
                   );
                 }
               });
             });
           });
           // 1 ~ 8등 계산
-          for(i = 1; i < 9; i++) {
-            rank(history_rank,i)
+          for (i = 1; i < 9; i++) {
+            rank(history_rank, i);
           }
           // 배열 평균 구하기
-          const arr_sum = history_rank.reduce(function add(sum, value){
+          const arr_sum = history_rank.reduce(function add(sum, value) {
             return sum + value;
           }, 0);
-          const arr_avg = arr_sum/history_rank.length
+          const arr_avg = arr_sum / history_rank.length;
 
           // TOP 횟수 구하기
           const top = rank_list[1] + rank_list[2] + rank_list[3];
 
-          $('#avg_rank').text('#'+arr_avg)
-          $('#first_rank').text(rank_list[0])
-          $('#top_rank').text(top)
+          $("#avg_rank").text("#" + arr_avg.toFixed(1));
+          $("#first_rank").text(rank_list[0]);
+          $("#top_rank").text(top);
           createChart();
         });
     });
 
-    // 최근 20판 등수별 개수 구하기
-    const rank = (arr, r) => {
-      var count = 0;
+  // 최근 20판 등수별 개수 구하기
+  const rank = (arr, r) => {
+    var count = 0;
 
-      for(let i = 0; i < arr.length; i++) {
-        if(arr[i] == r) {
-          count++;
-        }
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == r) {
+        count++;
       }
-      rank_list.push(count);
     }
+    rank_list.push(count);
+  };
 
   // 차트
   var chartArea = document.querySelector("canvas").getContext("2d");
