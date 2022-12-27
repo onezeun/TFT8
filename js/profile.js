74db6dbf725451b533cb9f53dae2153828a1b0a7
onezeun <onezeun@gmail.com>
2022-12-27 18:22:53 +0900

    api 키 수정

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var search_name = localStorage.getItem("search_name");
-  var api_key = "RGAPI-7f429815-4015-4952-8c97-5180fec38c2b";
+  var api_key = "RGAPI-b00d4381-1d39-4488-9be3-a46abd4f49ba";
 
   // 전역 변수 설정
   var user_id;
